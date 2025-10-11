068d3609786de0adac4133e6f009d66bf5dcee4e
dima.moisieienko <dimonmail09@gmail.com>
2025-10-11 18:54:23 +0300

    fixed router

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('jobs', 'JobController');
+Route::resource('jobs', JobController::class);
